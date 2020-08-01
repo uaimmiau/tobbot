e3f3f488d99b09a673d38ab3137c6da3fa7c8fee
Uaimmiau <zoltanchivay2137@gmail.com>
2020-08-01 22:54:01 +0200

    pseudo ulepszenia, pewnie coś jebnie

--- mebot.js
+++ mebot.js
@@ -1,35 +1,62 @@
+// This will check if the node version you are running is the required
+// Node version, if it isn't it will throw the following error to inform
+// you.
+if (Number(process.version.slice(1).split(".")[0]) < 8) throw new Error("Node 8.0.0 or higher is required. Update Node on your system.");
+
 const Discord = require("discord.js");
-const Enmap = require("enmap");
+const {
+  promisify
+} = require("util");
 const fs = require("fs");
+const readdir = promisify(require("fs").readdir);
+const Enmap = require("enmap");
 
 const client = new Discord.Client();
-const config = require("./config.json");
-client.config = config;
+client.config = require("./config.json");
 
-fs.readdir("./events/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    const event = require(`./events/${file}`);
-    let eventName = file.split(".")[0];
-    client.on(eventName, event.bind(null, client));
-  });
-});
 
+// Aliases and commands are put in collections where they can be read from,
+// catalogued, listed, etc.
 client.commands = new Enmap();
 
-fs.readdir("./commands/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/${file}`);
-    let commandName = file.split(".")[0];
-    console.log(`Attempting to load command ${commandName}`);
-    client.commands.set(commandName, props);
+// Now we integrate the use of Evie's awesome EnMap module, which
+// essentially saves a collection to disk. This is great for per-server configs,
+// and makes things extremely easy for this purpose.
+
+
+// We're doing real fancy node 8 async/await stuff here, and to do that
+// we need to wrap stuff in an anonymous function. It's annoying but it works.
+
+const init = async () => {
+
+  // Here we load **commands** into memory, as a collection, so they're accessible
+  // here and everywhere else.
+  fs.readdir("./commands/", (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(file => {
+      if (!file.endsWith(".js")) return;
+      let props = require(`./commands/${file}`);
+      let commandName = file.split(".")[0];
+      console.log(`Attempting to load command ${commandName}`);
+      client.commands.set(commandName, props);
+    });
   });
-});
+  // Then we load events, which will include our message and ready event.
+  const evtFiles = await readdir("./events/");
+  console.log(`Loading a total of ${evtFiles.length} events.`);
+  evtFiles.forEach(file => {
+    const eventName = file.split(".")[0];
+    console.log(`Loading Event: ${eventName}`);
+    const event = require(`./events/${file}`);
+    // Bind the client to any event, before the existing arguments
+    // provided by the discord.js event. 
+    // This line is awesome by the way. Just sayin'.
+    client.on(eventName, event.bind(null, client));
+  });
 
-client.on("error", (e) => console.error(e));
-client.on("warn", (e) => console.warn(e));
-client.on("debug", (e) => console.info(e));
+  client.login(client.config.token);
 
-client.login(config.token);
+  // End top-level async/await function.
+};
+
+init();
